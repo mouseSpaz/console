e4b921d9d31459daa7f89d0efa31744e9b25e5db
Chris Keefer <chriskeefer@Chriss-MacBook-Pro.local>
2018-09-22 22:45:55 -0700

    cleanup and fix pipe for getTeams.

--- src/app/team.service.ts
+++ src/app/team.service.ts
@@ -1,31 +1,52 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { MessageService } from './message.service';
-import { Team } from './Team';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Team } from './Team';
+import { MessageService } from './message.service';
+
+@Injectable({ providedIn: 'root' })
 export class TeamService {
+
   private teamsUrl = 'http://mousespaz.engine/teams';
 
   constructor(
     private messageService: MessageService,
-    private http: HttpClient
-  ) { }
+    private http: HttpClient) { }
+
+  /** GET teams from the server */
+  getTeams (): Observable<Team[]> {
+  return this.http.get<Team[]>(this.teamsUrl)
+    .pipe(
+      map(res => res['data']),
+      tap(teams => this.log('Fetched teams.')),
+      catchError(this.handleError('getTeams', []))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 
   private log(message: string) {
     this.messageService.add('TeamService: $(message)');
-  }
-
-  getTeams(): Observable<Team[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return this.http.get<Team[]>(this.teamsUrl).pipe(
-      map((response: Response) => {
-        return response.data;
-      }
-    ));
   }
 }

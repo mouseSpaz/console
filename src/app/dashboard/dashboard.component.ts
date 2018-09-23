0a7c5583c121b6362e30ad38a4b47c591e902fa7
Chris Keefer <chriskeefer@Chriss-MacBook-Pro.local>
2018-09-23 14:53:54 -0700

    display list of teams and current users name

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService} from '../services/authentication.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  username: string = null;
+  constructor(
+    private auth: AuthenticationService) { }
 
   ngOnInit() {
+    this.auth.getSelf().subscribe(user => {
+      this.username = user.username;
+    });
   }
 
 }
